984471d793e41f5bbb9176c2464572229635ff04
and-z <mail4andz@gmail.com>
2012-08-21 23:15:39 +0200

    Added view by ID controller action; Added timestamp for new posts.

--- src/controller/cb_blog_post_controller.erl
+++ src/controller/cb_blog_post_controller.erl
@@ -3,7 +3,14 @@
 
 view('GET', []) ->
 	Posts = boss_db:find(post, []),
-	{ok, [{posts, Posts}]}.
+	{ok, [{posts, Posts}]};
+view('GET', [Id]) ->
+	case boss_db:find(Id) of
+		Post ->
+	 		{ok, [{posts, [Post|[]]}]};
+	 	{error, Reason} ->
+	 		error
+	 end.
 
 create('GET', []) ->
 	Categories = boss_db:find(category, []),
@@ -13,7 +20,7 @@ create('POST', []) ->
 	PostText = Req:post_param("post_text"),
 	PostAuthor = Req:post_param("post_author"),
 	CategoryId = Req:post_param("category_id"),
-	Post = post:new(id, CategoryId, PostTitle, PostText, PostAuthor),
+	Post = post:new(id, CategoryId, PostTitle, PostText, PostAuthor, erlang:now()),
 	case Post:save() of
 		{ok, SavedPost} ->
 			{redirect, [{action, "view"}]};
